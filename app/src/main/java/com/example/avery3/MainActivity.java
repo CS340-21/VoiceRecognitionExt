1c7133abe7fadaad481d35f1dd73a419720f3156
peilin314 <peilin314159@gmail.com>
2021-04-25 19:19:53 -0400

    add new event

---
+++ app/src/main/java/com/example/avery3/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.avery3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewEvent();
+            }
+        });
+    }
+
+    public void openNewEvent() {
+        Intent intent = new Intent(this, addEvent.class);
+        startActivity(intent);
+    }
+}
