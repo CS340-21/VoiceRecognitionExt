e60f4dbd1d2e742b565e6e8b4340bee1dd340bda
peilin314 <peilin314159@gmail.com>
2021-04-25 23:12:51 -0400

    save button refers back to main page

--- app/src/main/java/com/example/avery3/addEvent.java
+++ app/src/main/java/com/example/avery3/addEvent.java
@@ -2,13 +2,33 @@ package com.example.avery3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class addEvent extends AppCompatActivity {
+    private Button button;
+    private TextInputLayout eventName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        button = (Button) findViewById(R.id.saveButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNewEvent();
+            }
+        });
+    }
+
+    public void saveNewEvent() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
