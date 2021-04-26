a9623520665c74a14de6a6b8e89d08f1b2efd3de
Andy Vo <andyv0110@gmail.com>
2021-04-26 06:17:46 -0400

    add template for CustomAdaptor to use with a RecyclerView in order to see a list of buttons that will send them to the event information when clicked on

--- app/src/main/java/com/example/avery3/MainActivity.java
+++ app/src/main/java/com/example/avery3/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.avery3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SortedList;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.view.View;
+import android.view.*;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    RecyclerView recycle;
+    SortedList<String> buttonNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +31,11 @@ public class MainActivity extends AppCompatActivity {
     public void openNewEvent() {
         Intent intent = new Intent(this, addEvent.class);
         startActivity(intent);
+    }
+
+    //Intended to create a RecyclerView in order to set up a list of buttons,
+    //look at my implementation and see if you can figure out why it didn't work for me lol
+    public void createRecyclerView() {
+        CustomAdaptor tmp = new CustomAdaptor(buttonNames);
     }
 }
